2abbaa1e936fabeda152be14ec8e43b24ee3ac57
Joe Sydlowski <syd235@gmail.com>
2022-01-07 20:06:55 -0600

    redo data loads for 2018 forward to test ep load

--- R/save_ep_data.R
+++ R/save_ep_data.R
@@ -3,21 +3,23 @@ save_ep_data <- function(season){
   ep_object <- ffexpectedpoints::ep_build(season)
   
   # rds
-  saveRDS(ep_object$weekly, glue::glue('data/weekly/ep_weekly_{season}.rds'))
-  saveRDS(ep_object$pbp$rush_df, glue::glue('data/pbp/ep_pbp_rush_{season}.rds'))
-  saveRDS(ep_object$pbp$pass_df, glue::glue('data/pbp/ep_pbp_pass_{season}.rds'))
+  saveRDS(ep_object$ep_weekly, glue::glue('data/weekly/ep_weekly_{season}.rds'))
+  saveRDS(ep_object$ep_pbp_rush, glue::glue('data/pbp/ep_pbp_rush_{season}.rds'))
+  saveRDS(ep_object$ep_pbp_pass, glue::glue('data/pbp/ep_pbp_pass_{season}.rds'))
   
   # csv
-  readr::write_csv(ep_object$weekly, glue::glue('data/weekly/ep_weekly_{season}.csv'))
-  readr::write_csv(ep_object$pbp$rush_df, glue::glue('data/pbp/ep_pbp_rush_{season}.csv'))
-  readr::write_csv(ep_object$pbp$pass_df, glue::glue('data/pbp/ep_pbp_pass_{season}.csv'))
+  readr::write_csv(ep_object$ep_weekly, path = glue::glue('data/weekly/ep_weekly_{season}.csv'))
+  readr::write_csv(ep_object$ep_pbp_rush, path = glue::glue('data/pbp/ep_pbp_rush_{season}.csv'))
+  readr::write_csv(ep_object$ep_pbp_pass, path = glue::glue('data/pbp/ep_pbp_pass_{season}.csv'))
   
   # .parquet
-  arrow::write_parquet(ep_object$weekly, glue::glue('data/weekly/ep_weekly_{season}.parquet'))
-  arrow::write_parquet(ep_object$pbp$rush_df, glue::glue('data/pbp/ep_pbp_rush_{season}.parquet'))
-  arrow::write_parquet(ep_object$pbp$pass_df, glue::glue('data/pbp/ep_pbp_pass_{season}.parquet'))
+  arrow::write_parquet(ep_object$ep_weekly, glue::glue('data/weekly/ep_weekly_{season}.parquet'))
+  arrow::write_parquet(ep_object$ep_pbp_rush, glue::glue('data/pbp/ep_pbp_rush_{season}.parquet'))
+  arrow::write_parquet(ep_object$ep_pbp_pass, glue::glue('data/pbp/ep_pbp_pass_{season}.parquet'))
   
   cli::cli_alert_success("Saved {season} weekly and pbp data.")
   
 return(invisible())
 }
+
+# purrr::walk(.x = 2018:2021, .f = ~save_ep_data(season = .x))
